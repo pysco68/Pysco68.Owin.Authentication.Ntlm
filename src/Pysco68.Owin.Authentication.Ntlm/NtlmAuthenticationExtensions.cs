c1b451500576d5205daea86c4e2582d05481ee60
Yannic Staudt <yannic.staudt@ygraphix.net>
2014-12-19 10:53:00 +0100

    Added extension method to ease use of the authentication middleware

--- src/Pysco68.Owin.Authentication.Ntlm/NtlmAuthenticationExtensions.cs
+++ src/Pysco68.Owin.Authentication.Ntlm/NtlmAuthenticationExtensions.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Owin;
+using Microsoft.Owin.Extensions;
+using System;
 
 namespace Pysco68.Owin.Authentication.Ntlm
 {
     public static class NtlmAuthenticationExtensions
     {
+        /// <summary>
+        /// Enable using Ntlm authentication
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static IAppBuilder UseNtlmAuthentication(this IAppBuilder app, NtlmAuthenticationOptions options = null)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException("app");
+            }
+
+            app.Use(typeof(NtlmAuthenticationMiddleware), app, options != null ? options : new NtlmAuthenticationOptions());
+            app.UseStageMarker(PipelineStage.Authenticate);
+
+            return app;
+        }
     }
 }
