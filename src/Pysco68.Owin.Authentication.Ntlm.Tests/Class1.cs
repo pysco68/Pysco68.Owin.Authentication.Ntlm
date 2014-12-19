9b9e5fcce5f849ee1631ae0201a7ee1a147b2ca9
Yannic Staudt <yannic.staudt@ygraphix.net>
2014-12-19 10:45:00 +0100

    Preparing test framework

---
+++ src/Pysco68.Owin.Authentication.Ntlm.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pysco68.Owin.Authentication.Ntlm.Tests
+{
+    public class Class1
+    {
+    }
+}
